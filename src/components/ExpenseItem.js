eec31324bf310b15b319fc55a1cfd20c37a3a340
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-12-16 14:18:35 +0500

    new expense

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  
   return (
     <div className="expense-item">
-      <div> December 16, 2023</div>
+      <div style={{ color: "white" }}> {props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">Rs. 4000/-</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs. {props.amount}/-</div>
       </div>
     </div>
   );
